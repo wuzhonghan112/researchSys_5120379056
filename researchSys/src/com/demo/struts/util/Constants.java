ad2e516a1cc2931b23ff9aedc3982b7e72876707
Hanzhong <wuzhonghan112@outlook.com>
2015-07-06 14:43:25 +0800

    Revert "2015/7/6"
    This reverts commit 173631f1034c8c31af1aa2304c910cb1dd2c1ccb.

--- researchSys/src/com/demo/struts/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.demo.struts.util;
-
-public class Constants {
-	public final static String USERNAME_KEY = "username";
-
-	public final static String SUCCESS_KEY = "success";
-	public final static String FAILURE_KEY = "failure";
-	public final static String LOGOUT_KEY = "logout";
-}
